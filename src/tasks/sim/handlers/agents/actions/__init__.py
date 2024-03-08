833d503913f2c0842df0a8b8a8bbc3fc48c8b25b
Ray7788 <ray778@foxmail.com>
2024-03-08 18:02:09 +0000

    Add agent actions import

---
+++ src/tasks/sim/handlers/agents/actions/__init__.py
@@ -0,0 +1,7 @@
+from camera import *
+from craft import *
+from minerl.herobraine.hero.handlers.agent.actions import action
+from minerl.herobraine.hero.handlers.agent.actions import place
+from minerl.herobraine.hero.handlers.agent.actions import keyboard
+from smelt import *
+from swap_slot import SwapSlotAction
